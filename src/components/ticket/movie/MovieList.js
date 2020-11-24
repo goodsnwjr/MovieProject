34c3170d573953e5e47e44538afb47ab7108bad7
eunJeong <poilchxn@imform.co.kr>
2020-11-24 17:18:09 +0900

    티켓 영화 리스트 받아오기

--- src/components/ticket/movie/MovieList.js
+++ src/components/ticket/movie/MovieList.js
@@ -1,41 +1,25 @@
 import React, { useEffect, useState } from 'react';
 
 //modulse
-import { Row, Col, Radio } from 'antd';
-import axios from 'axios';
+import { Col, Radio } from 'antd';
 
 const MovieList = (props) => {
-  const { MovieAPI, movie, setMovie, today, chooseMovie, setDecide, setError, setLoading } = props;
+	const { MovieAPI, chooseMovie } = props;
+	let movieList = MovieAPI && MovieAPI.dailyBoxOfficeList;
 
-  useEffect(() => {
-    const fetchMovie = async () => {
-      try {
-        setError(null);
-        setLoading(true);
-        setMovie(MovieAPI.dailyBoxOfficeList);
-      } catch (e) {
-        // setError(e);
-      }
-      // setLoading(false);
-    };
-    fetchMovie();
-  }, []);
-
-  let movieList = MovieAPI && MovieAPI.dailyBoxOfficeList;
-
-  return (
-    <Radio.Group defaultValue="a" size="large">
-      {MovieAPI &&
-        movieList.map((ele, idx) => {
-          return (
-            <Radio.Button value={ele.movieNm} onChange={(e) => chooseMovie(e)} key={`movie-list-${idx}`} name="movie">
-              <Col>{ele.age}</Col>
-              <Col>{ele.movieNm}</Col>
-            </Radio.Button>
-          );
-        })}
-    </Radio.Group>
-  );
+	return (
+		<Radio.Group defaultValue="a" size="large" name="movie">
+			{MovieAPI &&
+				movieList.map((ele, idx) => {
+					return (
+						<Radio.Button value={ele.movieNm} onClick={(e) => chooseMovie(e)} key={`movie-list-${idx}`}>
+							<Col>{ele.age}</Col>
+							<Col>{ele.movieNm}</Col>
+						</Radio.Button>
+					);
+				})}
+		</Radio.Group>
+	);
 };
 
 export default MovieList;
