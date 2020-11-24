5acfa090a86e5bdc5f464446e32d819f46623517
eunJeong <poilchxn@imform.co.kr>
2020-11-24 17:18:46 +0900

    영화관 리스트 받아고기

--- src/components/ticket/movie/TheaterList.js
+++ src/components/ticket/movie/TheaterList.js
@@ -1,46 +1,42 @@
-import React,{useEffect, useState} from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 
 //modulse
 import styled from 'styled-components';
-import {Row, Col, Cascader} from 'antd';
-
+import { Row, Col, Cascader } from 'antd';
 
 const TheaterList = (props) => {
-  const {TheaterAPI, theater, setTheater, today, chooseMovie, setDecide, setError, setLoading} = props;
-  console.log(TheaterAPI);
-  useEffect(()=> {
-    const fetchTheater = async () => {
-      try {
-        setError(null);
-        setLoading(true);
-        setTheater(TheaterAPI.Theater);
-      } catch (e) {
-        // setError(e);
-      } 
-      // setLoading(false);
-    }
-    fetchTheater();
-  }, []);
-  function onChange(value) {
-    console.log(value);
-  }
-  
-  const TheaterStyle = styled(Col)`
-    display: block;
-  `
+	const { TheaterAPI, theater, setTheater, today, chooseTheater, setDecide, setError, setLoading } = props;
+	console.log(TheaterAPI);
+	useEffect(() => {
+		const fetchTheater = async () => {
+			try {
+				setError(null);
+				setLoading(true);
+				setTheater(TheaterAPI.Theater);
+			} catch (e) {
+				// setError(e);
+			}
+			// setLoading(false);
+		};
+		fetchTheater();
+	}, []);
 
-  return (
-    <>
-      {
-        TheaterAPI.Theater && 
-        <Cascader 
-          options={TheaterAPI.Theater}
-          onChange={onChange} 
-          defaultValue={['서울', '강변', '10: 30']}
-        />
-      }
-    </>
-  ) 
+	const TheaterStyle = styled(Col)`
+		display: block;
+	`;
+
+	return (
+		<>
+			{TheaterAPI.Theater && (
+				<Cascader
+					className="theater"
+					options={TheaterAPI.Theater}
+					onChange={(value) => chooseTheater(value)}
+					defaultValue={['서울', '강변', '10: 30']}
+				/>
+			)}
+		</>
+	);
 };
 
 export default TheaterList;
