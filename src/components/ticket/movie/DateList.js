d6e9b853d0dd755c28b6e208145b25e2f7e57047
eunJeong <poilchxn@imform.co.kr>
2020-11-25 15:29:30 +0900

    날짜 리스트

--- src/components/ticket/movie/DateList.js
+++ src/components/ticket/movie/DateList.js
@@ -3,9 +3,7 @@ import React, { useState, useEffect } from 'react';
 //modulse
 import styled from 'styled-components';
 
-import { ko } from 'date-fns/locale';
-
-import { dayList } from 'modules/movieList';
+import { monthList, dayList } from 'modules/movieList';
 import DatePicker from 'react-horizontal-datepicker';
 import './DateList.css';
 
@@ -24,11 +22,13 @@ const DateList = (props) => {
 	};
 
 	useEffect(() => {
-		// customCalender(document.querySelectorAll('.DatePicker_monthYearLabel__137fh'), monthList);
+		customCalender(document.querySelectorAll('.DatePicker_monthYearLabel__137fh'), monthList);
 		customCalender(document.querySelectorAll('.DatePicker_dayLabel__3gxfP'), dayList);
 	}, []);
 
-	return <DatePicker labelFormat={'M'} getSelectedDay={(value) => chooseDate(value)} color={'#374e8c'} endDate="60" />;
+	return (
+		<DatePicker labelFormat={'MMMM'} getSelectedDay={(value) => chooseDate(value)} color={'#374e8c'} endDate="60" />
+	);
 };
 
 export default DateList;
