f7feca7e6d7b6b3d22c54317b0f06ad942682554
이현성 <goodsnwjr2@naver.com>
2020-11-26 14:08:28 +0900

    폴더 나눔

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const components = () => {
-  return <div>컴포넌트 폴더 생성</div>;
-};
-
-export default components;
--- src/lib/layout/styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const MainLayout = styled.div`
-  width: 60%;
-  height: 100%;
-  margin: 64px auto 0px;
-`
